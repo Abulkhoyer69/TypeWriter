12d961ca92c93d2314824415c488ba39c21d1f97
Abul Khoyer ⚡️ <abulkhoyer69@gmail.com>
2020-10-23 17:11:11 +0600

    Added files

---
+++ app.js
@@ -0,0 +1,51 @@
+/*!
+ *
+ *   TypeWriter - A Modern JavaScript Typing Animation Library
+ *   Author: Abul Khoyer <abulkhoyer.com>
+ *   Version: v1.0.0
+ *   Url:
+ *   License(s): MIT
+ *
+ */
+
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement
+    this.words = words
+    this.txt = ''
+    this.wordIndex = 0
+    this.wait = parseInt(wait, 10)
+    this.type()
+    this.isDeleting = false
+  }
+  type() {
+    const current = this.wordIndex % this.words.length
+    const fullTxt = this.words[current]
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1)
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1)
+    }
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+    let typeSpeed = 300
+    if (this.isDeleting) {
+      typeSpeed /= 2
+    }
+    if (!this.isDeleting && this.txt === fullTxt) {
+      typeSpeed = this.wait
+      this.isDeleting = true
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false
+      this.wordIndex++
+      typeSpeed = 500
+    }
+    setTimeout(() => this.type(), typeSpeed)
+  }
+}
+function init() {
+  const txtElement = document.querySelector('#typed')
+  const words = JSON.parse(txtElement.getAttribute('data-words'))
+  const wait = txtElement.getAttribute('data-wait')
+  new TypeWriter(txtElement, words, wait)
+}
+document.addEventListener('DOMContentLoaded', init)
